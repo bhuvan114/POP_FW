59a6e407e9b47b84eba694bb805b8da8ba02fb96
bhuvan114 <bhuvan114@users.noreply.github.com>
2016-03-19 11:57:49 -0400

    3P controls

---
+++ Partial Planner/Assets/scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = true;
+		}
+	}
+}
