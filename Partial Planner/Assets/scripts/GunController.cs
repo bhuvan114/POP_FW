f3d2a0942a74ed3a929d4769f13897a75a4e6d89
mpdwyer <mpdwyer93@gmail.com>
2016-03-20 14:33:18 -0400

    Added pistol asset

---
+++ Partial Planner/Assets/scripts/GunController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	//private Vector3 gunOffest;
+	private bool isHolding;
+	private Rigidbody rb;
+
+	public GameObject holder;
+	// Use this for initialization
+	void Start () {
+		isHolding = false;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isHolding) {
+			transform.position = holder.transform.position;
+			transform.rotation = holder.transform.rotation;
+		}
+	}
+
+	public void SetIsHolding(bool holding) {
+
+		isHolding = holding;
+	}
+
+	public void SetGravity(bool status) {
+
+		rb.useGravity = status;
+	}
+}
